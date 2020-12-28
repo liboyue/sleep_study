14fa4ab167c31cb13bb319fdd34245cd9e1ea6ee
harlin <harlin@cmu.edu>
2020-12-27 21:37:32 -0500

    initialize age list

--- sleep_study/__init__.py
+++ sleep_study/__init__.py
@@ -8,6 +8,8 @@ def init(tmp_dir='/ux0/data/NCH_Sleep_Data'):
     global data_dir
     data_dir = tmp_dir
     data.study_list = data.init_study_list()
+    age_fn = data.init_age_file()
+    print("age information stored in", age_fn)
 
     # Load the sleep study info
     info.SLEEP_STUDY = info.load_health_info(info.SLEEP_STUDY)
